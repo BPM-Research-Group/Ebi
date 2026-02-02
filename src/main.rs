98150003a897e157419432005b5b9fcaa0ebfcde
Your Name <you@example.com>
2026-02-02 19:25:50 +0100

    disable logging in release mode by default

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ use ebi;
 use ebi::ebi_framework::ebi_command::EBI_COMMANDS;
 
 pub fn main() -> Result<()> {
-
     //enable debugging
-    Builder::new().filter_level(LevelFilter::Trace).init();
+    if cfg!(debug_assertions) {
+        Builder::new().filter_level(LevelFilter::Trace).init();
+    }
 
     let command = EBI_COMMANDS.build_cli();
     let command = command.version(crate_version!());
     let cli_matches = command.get_matches();
-    
+
     log::info!("Ebi starting");
 
     EBI_COMMANDS.execute(&cli_matches).context("Executing Ebi")
