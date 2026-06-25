730751c9109c32bc3743077fd2e9a9f0bce665f5
CI pipeline <cipipeline@example.com>
2026-06-25 09:45:18 +0200

    towards split miner

--- src/techniques/split_miner.rs
+++ src/techniques/split_miner.rs
@@ -3,11 +3,14 @@ use crate::{
     techniques::stochastic_markovian_abstraction::MarkovianAbstraction,
 };
 use ebi_objects::{
-    Activity, BusinessProcessModelAndNotation, FiniteStochasticLanguage, HasActivityKey, ebi_arithmetic::{Fraction, Zero, f},
+    Activity, BusinessProcessModelAndNotation, FiniteStochasticLanguage, HasActivityKey,
+    IntoRefTraceIterator, IntoRefTraceProbabilityIterator,
+    ebi_arithmetic::{Fraction, Signed, Zero, f},
 };
 use intmap::IntMap;
+use itertools::Itertools;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     hash::Hash,
 };
 use uuid::Uuid;
@@ -72,7 +75,7 @@ pub fn split_miner(
     let (start_activity, end_activity) =
         MarkovianAbstraction::create_start_end(slang.activity_key_mut());
 
-    let dfgp = generateDFGP(log, parameters, start_activity, end_activity);
+    let dfgp = generateDFGP(&slang, parameters, start_activity, end_activity);
     let mut bpmnDiagram = transformDFGPintoBPMN(dfgp);
     if parameters.structuringTime == StructuringTime::Post {
         bpmnDiagram = structure(bpmnDiagram);
@@ -81,7 +84,7 @@ pub fn split_miner(
 }
 
 fn generateDFGP<'a>(
-    log: &'a dyn EbiTraitFiniteStochasticLanguage,
+    log: &'a FiniteStochasticLanguage,
     parameters: &'a SplitMinerParameters,
     start_activity: Activity,
     end_activity: Activity,
@@ -108,18 +111,20 @@ struct DirectlyFollowGraphPlus<'a> {
     loopsL1: HashSet<Activity>,
     loopsL1Freq: IntMap<Activity, Fraction>,
     loopsL2: HashSet<DFGEdge>,
-    parallelisms: IntMap<usize, HashSet<usize>>,
-    concurrencyMatrix: Vec<Fraction>,
+    parallelisms: IntMap<Activity, HashSet<Activity>>,
+    concurrencyMatrix: Option<Vec<Fraction>>,
     bestEdges: HashSet<DFGEdge>,
     untouchableEdges: Option<HashSet<DFGEdge>>,
     potentialConcurrency: HashSet<DFGEdge>,
-    relations: HashMap<(usize, usize), Gate>,
+    relations: Option<HashMap<(usize, usize), Gate>>,
 
     parameters: SplitMinerParameters,
 
     filterThreshold: Option<usize>,
 
     oracle: bool,
+
+    zero: Fraction,
 }
 
 impl<'a> DirectlyFollowGraphPlus<'a> {
@@ -136,21 +141,23 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
             edges: HashSet::new(),
             edge_2_frequency: HashMap::new(),
             nodes: IntMap::new(),
+            node_2_frequency: HashMap::new(),
             outgoings: IntMap::new(),
             incomings: IntMap::new(),
             dfgp: IntMap::new(),
             loopsL1: HashSet::new(),
             loopsL1Freq: IntMap::new(),
-            loopsL2: (),
-            parallelisms: (),
-            concurrencyMatrix: (),
-            bestEdges: (),
-            untouchableEdges: (),
-            potentialConcurrency: (),
-            relations: (),
+            loopsL2: HashSet::new(),
+            parallelisms: IntMap::new(),
+            concurrencyMatrix: None,
+            bestEdges: HashSet::new(),
+            untouchableEdges: None,
+            potentialConcurrency: HashSet::new(),
+            relations: None,
             parameters: parameters.clone(),
-            filterThreshold: (),
-            oracle: (),
+            filterThreshold: None,
+            oracle: false,
+            zero: Fraction::zero(),
         }
     }
 
@@ -158,11 +165,11 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
         self.untouchableEdges = None;
 
         self.buildDirectlyFollowsGraph();
-        detectLoops(); //depends on buildDirectlyFollowsGraph()
+        self.detectLoops(); //depends on buildDirectlyFollowsGraph()
         if self.oracle {
             detectRelationsOnLog()
         } else {
-            detectParallelismsOnDFG()
+            self.detectParallelismsOnDFG()
         } //depends on detectLoops()
 
         match self.parameters.filterType {
@@ -182,33 +189,32 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
     }
 
     fn buildDirectlyFollowsGraph(&mut self) {
-        let autogenStart = DFGNode::new(self.startcode);
-        self.addNode(autogenStart.clone());
+        let autogen_start = DFGNode::new(self.startcode);
+        self.addNode(autogen_start.clone());
         //        while parsing the simple log we will always skip the start event,
         //        so we set now the maximum frequency because it is an artificial start event
-        self.increase_node_frequency(autogenStart.id, &self.log.get_probability_sum());
-        autogenStart.increaseFrequency(&f!(self.log.number_of_traces()));
+        self.increase_node_frequency(autogen_start.id, &self.log.get_probability_sum());
 
-        let autogenEnd = DFGNode::new(self.endcode);
-        self.addNode(autogenEnd);
+        let autogen_end = DFGNode::new(self.endcode);
+        self.addNode(autogen_end);
 
-        for (trace, traceFrequency) in self.log.iter_traces_probabilities() {
+        for (trace, trace_frequency) in self.log.iter_traces_probabilities() {
             let mut trace_it = trace.iter();
             //            consuming the start event that is always 0
             trace_it.next();
 
-            let prevEvent = self.startcode;
-            let prevNode = autogenStart;
+            let mut prevEvent = self.startcode;
+            let mut prevNode = autogen_start.clone();
 
             while let Some(event) = trace_it.next() {
                 //                we read the next event of the trace until it is finished
 
                 if prevEvent == *event {
                     if self.loopsL1.contains(event) {
-                        *self.loopsL1Freq.get_mut(*event).unwrap() += traceFrequency;
+                        *self.loopsL1Freq.get_mut(*event).unwrap() += trace_frequency;
                     } else {
                         self.loopsL1.insert(*event);
-                        self.loopsL1Freq.insert(*event, traceFrequency.clone());
+                        self.loopsL1Freq.insert(*event, trace_frequency.clone());
                     }
                     continue;
                 }
@@ -222,18 +228,18 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
                 }
 
                 //                  increasing frequency of this event occurrence
-                self.increase_node_frequency(node.id, traceFrequency);
+                self.increase_node_frequency(node.id, trace_frequency);
 
                 if !self.dfgp.contains_key(prevEvent)
                     || !self.dfgp.get(prevEvent).unwrap().contains_key(*event)
                 {
-                    let edge = DFGEdge::new(prevNode, node);
-                    self.addEdge(edge, traceFrequency.clone());
+                    let edge = DFGEdge::new(prevNode, node.clone());
+                    self.addEdge(edge, trace_frequency.clone());
                 }
 
                 //                  increasing frequency of this directly following relationship
                 let edge_id = self.dfgp.get(prevEvent).unwrap().get(*event).unwrap().id;
-                self.increase_edge_frequency(edge_id, traceFrequency);
+                self.increase_edge_frequency(edge_id, trace_frequency);
 
                 prevEvent = *event;
                 prevNode = node;
@@ -241,10 +247,134 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
         }
     }
 
+    fn detectLoops(&mut self) {
+        self.loopsL2.clear();
+        for e1 in &self.edges {
+            let src = e1.getSourceCode();
+            let tgt = e1.getTargetCode();
+
+            //            if src OR tgt are length 1 loops, we do not evaluate length 2 loops for this edge,
+            //            because a length 1 loop in parallel with something else
+            //            can generate pattern of the type [src :: tgt :: src] OR [tgt :: src :: tgt]
+            if !self.loopsL2.contains(e1)
+                && self.dfgp.get(tgt).unwrap().contains_key(src)
+                && !self.loopsL1.contains(&src)
+                && !self.loopsL1.contains(&tgt)
+            {
+                let e2 = self.dfgp.get(tgt).unwrap().get(src).unwrap();
+
+                let src2tgt_loop2_pattern = vec![src, tgt, src];
+                let tgt2src_loop2_pattern = vec![tgt, src, tgt];
+                let mut src2tgt_loop2_frequency = 0;
+                let mut tgt2src_loop2_frequency = 0;
+
+                for trace in self.log.iter_traces() {
+                    src2tgt_loop2_frequency += trace
+                        .windows(3)
+                        .filter(|x| *x == src2tgt_loop2_pattern)
+                        .count();
+                    tgt2src_loop2_frequency += trace
+                        .windows(3)
+                        .filter(|x: &&[Activity]| *x == tgt2src_loop2_pattern)
+                        .count();
+                }
+
+                let loop2score = src2tgt_loop2_frequency + tgt2src_loop2_frequency;
+
+                //                if the loop2score is not zero, it means we found patterns of the type:
+                //                [src :: tgt :: src] OR [tgt :: src :: tgt], so we set both edges as short-loops
+                if loop2score != 0 {
+                    self.loopsL2.insert(e1.clone());
+                    self.loopsL2.insert(e2.clone());
+                }
+            }
+        }
+    }
+
+    fn detectParallelismsOnDFG(&mut self) {
+        let mut removable_edges = HashSet::new();
+
+        if self.parameters.parallelismsThreshold.is_zero() {
+            return;
+        }
+
+        for e1 in &self.edges {
+            let src = e1.getSourceCode();
+            let tgt = e1.getTargetCode();
+
+            let priority_check = if self.parameters.parallelismsFirst {
+                !self.loopsL2.contains(&e1)
+            } else {
+                !self.loopsL2.contains(&e1)
+                    && !self.loopsL1.contains(&src)
+                    && !self.loopsL1.contains(&tgt)
+            };
+
+            if self.dfgp.get(tgt).unwrap().contains_key(src)
+                && priority_check
+                && !removable_edges.contains(&e1)
+            {
+                //                this means: src || tgt is candidate parallelism
+                let e2 = self.dfgp.get(tgt).unwrap().get(src).unwrap();
+
+                let src2tgt_frequency = self.get_edge_frequency(&e1);
+                let tgt2src_frequency = self.get_edge_frequency(&e2);
+                let parallelism_score = (src2tgt_frequency - tgt2src_frequency)
+                    / (src2tgt_frequency + tgt2src_frequency);
+
+                if parallelism_score.clone().abs() < self.parameters.parallelismsThreshold {
+                    //                    if parallelismScore is less than the threshold epslon,
+                    //                    we set src || tgt and vice-versa, and we remove e1 and e2
+                    if !self.parallelisms.contains_key(src) {
+                        self.parallelisms.insert(src, HashSet::new());
+                    }
+                    self.parallelisms.get_mut(src).unwrap().insert(tgt);
+                    if !self.parallelisms.contains_key(tgt) {
+                        self.parallelisms.insert(tgt, HashSet::new());
+                    }
+                    self.parallelisms.get_mut(tgt).unwrap().insert(src);
+                    removable_edges.insert(e1);
+                    removable_edges.insert(e2);
+                //                        totalParallelisms+=2;
+                } else {
+                    //                    otherwise we remove the least frequent edge, e1 or e2
+                    if parallelism_score.is_positive() {
+                        removable_edges.insert(e2);
+                    } else {
+                        removable_edges.insert(e1);
+                    }
+                    //                        notParallel++;
+                }
+            }
+        }
+
+        let mut orderedRemovableEdges = removable_edges.into_iter().cloned().collect::<Vec<_>>();
+        orderedRemovableEdges.sort();
+        for re in orderedRemovableEdges {
+            if !self.removeEdge(&re, true) {
+                //                System.out.println("DEBUG - impossible remove: " + re.print());
+                let src = re.getSourceCode();
+                let tgt = re.getTargetCode();
+                if self.parallelisms.contains_key(src) {
+                    self.parallelisms.get_mut(src).unwrap().remove(&tgt);
+                }
+                if self.parallelisms.contains_key(tgt) {
+                    self.parallelisms.get_mut(tgt).unwrap().remove(&src);
+                }
+                if let Some(e) = self.dfgp.get(tgt).unwrap().get(src)
+                    && *e == re
+                {
+                    self.removeEdge(&re, true);
+                }
+            }
+            //            else { confirmedParallelisms++; }
+        }
+    }
+
     fn addNode(&mut self, n: DFGNode) {
         let code = n.code;
 
-        self.nodes.insert(code, n);
+        self.nodes.insert(code, n.clone());
         if !self.incomings.contains_key(code) {
             self.incomings.insert(code, HashSet::new());
         }
@@ -265,7 +395,29 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
         self.incomings.get_mut(tgt).unwrap().insert(e.clone());
         self.outgoings.get_mut(src).unwrap().insert(e.clone());
         self.dfgp.get_mut(src).unwrap().insert(tgt, e.clone());
-        self.edge_2_frequency.insert(e, frequency);
+        self.edge_2_frequency.insert(e.id, frequency);
+    }
+
+    fn removeEdge(&mut self, e: &DFGEdge, safe: bool) -> bool {
+        let src = e.getSourceCode();
+        let tgt = e.getTargetCode();
+        if let Some(untouchableEdges) = &self.untouchableEdges
+            && untouchableEdges.contains(&e)
+        {
+            // System.out.println("DEBUG - this edge ensures connectedness! not removable!");
+            return false;
+        }
+        if safe && (self.incomings.get(tgt).unwrap().len() == 1)
+            || (self.outgoings.get(src).unwrap().len() == 1)
+        {
+            return false;
+        }
+        self.incomings.get_mut(tgt).unwrap().remove(&e);
+        self.outgoings.get_mut(src).unwrap().remove(&e);
+        self.dfgp.get_mut(src).unwrap().remove(tgt);
+        self.edges.remove(&e);
+        return true;
+        //        System.out.println("DEBUG - removed edge: " + src + " -> " + tgt);
     }
 
     fn increase_edge_frequency(&mut self, e: Uuid, frequency: &Fraction) {
@@ -274,6 +426,10 @@ impl<'a> DirectlyFollowGraphPlus<'a> {
 
     fn increase_node_frequency(&mut self, n: Uuid, frequency: &Fraction) {
         *self.node_2_frequency.get_mut(&n).unwrap() += frequency;
+    }
+
+    fn get_edge_frequency(&self, edge: &DFGEdge) -> &Fraction {
+        self.edge_2_frequency.get(&edge.id).unwrap_or(&self.zero)
     }
 }
 
@@ -320,24 +476,30 @@ impl Hash for DFGEdge {
     }
 }
 
+impl Ord for DFGEdge {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.id.cmp(&other.id)
+    }
+}
+
+impl PartialOrd for DFGEdge {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.id.partial_cmp(&other.id)
+    }
+}
+
 #[derive(Clone)]
 struct DFGNode {
     id: Uuid,
     code: Activity,
-
-    frequency: Fraction,
-    startFrequency: usize,
-    endFrequency: usize,
 }
 
 impl DFGNode {
     fn new(code: Activity) -> Self {
-        id: Uuid::new_v4(),
-        code
-    }
-
-    fn increaseFrequency(&mut self, amount: &Fraction) {
-        self.frequency += amount;
+        Self {
+            id: Uuid::new_v4(),
+            code,
+        }
     }
 }
 
